2df94a29a3143a54fc9ac220a532a4e293fd5579
timnickname <legofreund2@web.de>
2020-05-21 19:50:59 +0200

    added label for the bottom

--- Chatduell/Chatduell.py
+++ Chatduell/Chatduell.py
@@ -4,22 +4,18 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
+def window():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    height = 500
+    width = 500
+    window.setGeometry(200,200,width,height)
+    window.setWindowTitle("Chatduell")
+    label = QLabel(window)
+    label.setText("100")
+    label.move(width/2,height-50)
+    
+    window.show()
+    sys.exit(app.exec_())
 
-class MainWindow(QMainWindow):
-    def __init__(self, *args, **kwargs):
-        super(MainWindow,self).__init__(*args,**kwargs)
-
-        self.setWindowTitle("Chatduell")
-
-        label = QLabel("Testlabel")
-        label.setAlignment(Qt.AlignCenter)
-        self.setCentralWidget(label)
-
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-
-app.exec_()
+window()
