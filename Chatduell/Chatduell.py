a4ee75b0d9b60381f38a48e17eedd950cfe1ec58
timnickname <legofreund2@web.de>
2020-05-21 16:42:53 +0200

    21.05.2020 / 16:40

--- Chatduell/Chatduell.py
+++ Chatduell/Chatduell.py
@@ -4,6 +4,14 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
+
+class MainWindow(QMainWindow):
+    pass
+
 app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
 
 app.exec_()
